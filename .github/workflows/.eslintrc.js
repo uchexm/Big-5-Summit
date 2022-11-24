d1a9f323cd923bc3d123aeedeb77c8e54b008fcb
uchexm <109084069+uchexm@users.noreply.github.com>
2022-11-24 15:26:26 +0200

    Fix eslint problems

--- .github/workflows/.eslintrc.js
+++ .github/workflows/.eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
       "sourceType": "module"
   },
   "rules": {
-    "linebreak-style": ["error", "windows"]
-
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
   }
 };
